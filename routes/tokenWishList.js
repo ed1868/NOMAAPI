3bdf99a3f081218dd0ae6519d7806d3cb7fc5125
ed1868 <ruizeduardo21@gmail.com>
2021-06-15 14:48:33 -0400

    both email endpoints for token and contact working again with admin emials and user emails

--- routes/tokenWishList.js
+++ routes/tokenWishList.js
@@ -20,6 +20,337 @@ const transporter = nodemailer.createTransport({
 
 });
 
+let emailAdmin = (emailData) => {
+  let email = emailData.email;
+
+  let firstName = emailData.firstName;
+  
+
+  console.log("EMAILLL ADMIN : ", email);
+
+  transporter.sendMail({
+    from: '"NEW TOKEN USER ADDED TO WAITLIST" <myawesome@project.com>',
+    to: "ainomadsmiami@gmail.com",
+    subject: "NEW AI NOMADS REQUEST",
+    html: `
+        <!doctype html>
+        <html xmlns="http://www.w3.org/1999/xhtml" xmlns:v="urn:schemas-microsoft-com:vml" xmlns:o="urn:schemas-microsoft-com:office:office">
+          <head>
+            <title>
+              
+            </title>
+            <!--[if !mso]><!-->
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <!--<![endif]-->
+            <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1">
+            <style type="text/css">
+              #outlook a { padding:0; }
+              body { margin:0;padding:0;-webkit-text-size-adjust:100%;-ms-text-size-adjust:100%; }
+              table, td { border-collapse:collapse;mso-table-lspace:0pt;mso-table-rspace:0pt; }
+              img { border:0;height:auto;line-height:100%; outline:none;text-decoration:none;-ms-interpolation-mode:bicubic; }
+              p { display:block;margin:13px 0; }
+            </style>
+            <!--[if mso]>
+            <xml>
+            <o:OfficeDocumentSettings>
+              <o:AllowPNG/>
+              <o:PixelsPerInch>96</o:PixelsPerInch>
+            </o:OfficeDocumentSettings>
+            </xml>
+            <![endif]-->
+            <!--[if lte mso 11]>
+            <style type="text/css">
+              .mj-outlook-group-fix { width:100% !important; }
+            </style>
+            <![endif]-->
+            
+            
+        <style type="text/css">
+          @media only screen and (min-width:480px) {
+            .mj-column-per-100 { width:100% !important; max-width: 100%; }
+          }
+        </style>
+        
+      
+            <style type="text/css">
+            
+            
+    
+        @media only screen and (max-width:480px) {
+          table.mj-full-width-mobile { width: 100% !important; }
+          td.mj-full-width-mobile { width: auto !important; }
+        }
+      
+            </style>
+            
+            
+          </head>
+          <body style="word-spacing:normal;background-color:#F4F4F4;">
+            
+            
+          <div
+             style="background-color:#F4F4F4;"
+          >
+            
+          
+          <!--[if mso | IE]><table align="center" border="0" cellpadding="0" cellspacing="0" class="" style="width:600px;" width="600" ><tr><td style="line-height:0px;font-size:0px;mso-line-height-rule:exactly;"><![endif]-->
+        
+          
+          <div  style="background:#000000;background-color:#000000;margin:0px auto;max-width:600px;">
+            
+            <table
+               align="center" border="0" cellpadding="0" cellspacing="0" role="presentation" style="background:#000000;background-color:#000000;width:100%;"
+            >
+              <tbody>
+                <tr>
+                  <td
+                     style="direction:ltr;font-size:0px;padding:20px 0;text-align:center;"
+                  >
+                    <!--[if mso | IE]><table role="presentation" border="0" cellpadding="0" cellspacing="0"><tr><td class="" style="vertical-align:top;width:600px;" ><![endif]-->
+                
+          <div
+             class="mj-column-per-100 mj-outlook-group-fix" style="font-size:0px;text-align:left;direction:ltr;display:inline-block;vertical-align:top;width:100%;"
+          >
+            
+          <table
+             border="0" cellpadding="0" cellspacing="0" role="presentation" style="vertical-align:top;" width="100%"
+          >
+            <tbody>
+              
+                  <tr>
+                    <td
+                       align="center" style="font-size:0px;padding:10px 25px;padding-bottom:30px;word-break:break-word;"
+                    >
+                      
+          <table
+             border="0" cellpadding="0" cellspacing="0" role="presentation" style="border-collapse:collapse;border-spacing:0px;"
+          >
+            <tbody>
+              <tr>
+                <td  style="width:180px;">
+                  
+          <img
+             height="auto" src="https://louisianacontract.com/wp-content/uploads/2021/06/ailogo.png" style="border:none;display:block;outline:none;text-decoration:none;height:auto;width:100%;font-size:13px;" title="" width="180"
+          />
+        
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        
+                    </td>
+                  </tr>
+                
+                  <tr>
+                    <td
+                       align="left" style="font-size:0px;padding:10px 25px;padding-top:0px;padding-bottom:0px;word-break:break-word;"
+                    >
+                      
+          <div
+             style="font-family:Arial, sans-serif;font-size:13px;line-height:22px;text-align:left;color:#55575d;"
+          ><p style="line-height: 18px; margin: 10px 0; text-align: center;font-size:14px;color:#1C99FE;font-family:'Times New Roman',Helvetica,Arial,sans-serif">Home&nbsp; &nbsp; &nbsp; &nbsp;| &nbsp; &nbsp; &nbsp; NFT Store &nbsp; &nbsp; &nbsp; &nbsp;| &nbsp; &nbsp; &nbsp; About</p></div>
+        
+                    </td>
+                  </tr>
+                
+            </tbody>
+          </table>
+        
+          </div>
+        
+              <!--[if mso | IE]></td></tr></table><![endif]-->
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+            
+          </div>
+        
+          
+          <!--[if mso | IE]></td></tr></table><table align="center" border="0" cellpadding="0" cellspacing="0" class="" style="width:600px;" width="600" ><tr><td style="line-height:0px;font-size:0px;mso-line-height-rule:exactly;"><![endif]-->
+        
+          
+          <div  style="background:#000000;background-color:#000000;margin:0px auto;max-width:600px;">
+            
+            <table
+               align="center" border="0" cellpadding="0" cellspacing="0" role="presentation" style="background:#000000;background-color:#000000;width:100%;"
+            >
+              <tbody>
+                <tr>
+                  <td
+                     style="direction:ltr;font-size:0px;padding:0 0 0 0;text-align:center;"
+                  >
+                    <!--[if mso | IE]><table role="presentation" border="0" cellpadding="0" cellspacing="0"><tr></tr></table><![endif]-->
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+            
+          </div>
+        
+          
+          <!--[if mso | IE]></td></tr></table><table align="center" border="0" cellpadding="0" cellspacing="0" class="" style="width:600px;" width="600" ><tr><td style="line-height:0px;font-size:0px;mso-line-height-rule:exactly;"><![endif]-->
+        
+          
+          <div  style="background:#000000;background-color:#000000;margin:0px auto;max-width:600px;">
+            
+            <table
+               align="center" border="0" cellpadding="0" cellspacing="0" role="presentation" style="background:#000000;background-color:#000000;width:100%;"
+            >
+              <tbody>
+                <tr>
+                  <td
+                     style="direction:ltr;font-size:0px;padding:0 0 0 0;text-align:center;"
+                  >
+                    <!--[if mso | IE]><table role="presentation" border="0" cellpadding="0" cellspacing="0"><tr><td class="" style="vertical-align:top;width:600px;" ><![endif]-->
+                
+          <div
+             class="mj-column-per-100 mj-outlook-group-fix" style="font-size:0px;text-align:left;direction:ltr;display:inline-block;vertical-align:top;width:100%;"
+          >
+            
+          <table
+             border="0" cellpadding="0" cellspacing="0" role="presentation" style="vertical-align:top;" width="100%"
+          >
+            <tbody>
+              
+                  <tr>
+                    <td
+                       align="left" style="font-size:0px;padding:10px 25px;padding-top:25px;padding-bottom:5px;word-break:break-word;"
+                    >
+                      
+          <div
+             style="font-family:Arial, sans-serif;font-size:13px;line-height:22px;text-align:left;color:#55575d;"
+          ><p style="line-height: 60px; text-align: center; margin: 10px 0;font-size:55px;color:#1C99FE;font-family:'Times New Roman',Helvetica,Arial,sans-serif"><b>SAVE THIS USER!</b></p></div>
+        
+                    </td>
+                  </tr>
+                
+                  <tr>
+                    <td
+                       align="left" style="font-size:0px;padding:10px 25px;padding-top:0px;padding-bottom:20px;word-break:break-word;"
+                    >
+                    </td>
+                  </tr>
+                
+            </tbody>
+          </table>
+        
+          </div>
+        
+              <!--[if mso | IE]></td></tr></table><![endif]-->
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+            
+          </div>
+        
+          
+          <!--[if mso | IE]></td></tr></table><table align="center" border="0" cellpadding="0" cellspacing="0" class="" style="width:600px;" width="600" ><tr><td style="line-height:0px;font-size:0px;mso-line-height-rule:exactly;"><![endif]-->
+        
+          
+          <div  style="background:#000000;background-color:#000000;margin:0px auto;max-width:600px;">
+            
+            <table
+               align="center" border="0" cellpadding="0" cellspacing="0" role="presentation" style="background:#000000;background-color:#000000;width:100%;"
+            >
+              <tbody>
+                <tr>
+                  <td
+                     style="direction:ltr;font-size:0px;padding:0 0 0 0;padding-bottom:40px;text-align:center;"
+                  >
+                    <!--[if mso | IE]><table role="presentation" border="0" cellpadding="0" cellspacing="0"><tr><td class="" style="vertical-align:top;width:600px;" ><![endif]-->
+                
+          <div
+             class="mj-column-per-100 mj-outlook-group-fix" style="font-size:0px;text-align:left;direction:ltr;display:inline-block;vertical-align:top;width:100%;"
+          >
+            
+          <table
+             border="0" cellpadding="0" cellspacing="0" role="presentation" style="vertical-align:top;" width="100%"
+          >
+            <tbody>
+              
+                  <tr>
+                    <td
+                       align="center" vertical-align="middle" style="font-size:0px;padding:10px 25px;padding-bottom:30px;word-break:break-word;"
+                    >
+                      
+          <table
+             border="0" cellpadding="0" cellspacing="0" role="presentation" style="border-collapse:separate;line-height:100%;"
+          >
+            <tr>
+              <td
+                 align="center" bgcolor="#fd4766" role="presentation" style="border:none;border-radius:3px;cursor:auto;mso-padding-alt:10px 25px;background:#fd4766;" valign="middle"
+              >
+                <p
+                   style="display:inline-block;background:#fd4766;color:#ffffff;font-family:Times New Roman, Helvetica, Arial, sans-serif;font-size:18px;font-weight:normal;line-height:120%;margin:0;text-decoration:none;text-transform:none;padding:10px 25px;mso-padding-alt:0px;border-radius:3px;"
+                >
+                <br>
+                <br>
+                <span style="color:#212020">Email : ${email}</span>
+                <br>
+                <br>
+  
+           <br>
+           <br>
+           <span style="color:#212020">Name : ${firstName}</span>
+
+
+                </p>
+              </td>
+            </tr>
+          </table>
+        
+                    </td>
+                  </tr>
+                
+                  <tr>
+                    <td
+                       align="left" style="font-size:0px;padding:10px 25px;padding-top:5px;padding-bottom:0px;word-break:break-word;"
+                    >
+                      
+          <div
+             style="font-family:Arial, sans-serif;font-size:13px;line-height:22px;text-align:left;color:#55575d;"
+          ></div>
+        
+                    </td>
+                  </tr>
+                
+            </tbody>
+          </table>
+        
+          </div>
+        
+              <!--[if mso | IE]></td></tr></table><![endif]-->
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+            
+          </div>
+        
+          
+          <!--[if mso | IE]></td></tr></table><![endif]-->
+        
+        
+          </div>
+        
+          </body>
+        </html>
+      `
+  })
+    .then(info => {
+      console.log(`${info}YOU SENT AN EMAIL`)
+      return info;
+    }
+    )
+    .catch(error => console.log(error));
+
+
+}
+
+
 
 
 
@@ -348,6 +679,10 @@ let emailSender = (emailData) => {
   })
     .then(info => console.log(`${info}YOU SENT AN EMAIL`))
     .catch(error => console.log(error));
+
+
+
+    emailAdmin(emailData);
 }
 
 //GET ROUTE TO GET ALL EMAILS FROM TOKEN WISHLIST 
@@ -382,36 +717,40 @@ router.post('/tokenRequest', (req, res, next) => {
   }
 
   console.log('THE NEW TOKEN : ', token);
+  let emailPayload = {
+    firstName, email, confirmationCode: token
+  }
+  emailSender(emailPayload);
 
-  TokenWaitList.findOne({ email }).then(user => {
-    if (user != null) {
-      res.status(409).json({ message: "User already on waitlist" });
-      return
-    } else {
+  // TokenWaitList.findOne({ email }).then(user => {
+  //   if (user != null) {
+  //     res.status(409).json({ message: "User already on waitlist" });
+  //     return
+  //   } else {
 
-      const newTokenWaitListUser = new TokenWaitList({
-        firstName, email, confirmationCode: token
-      });
-      return newTokenWaitListUser.save()
-    }
+  //     const newTokenWaitListUser = new TokenWaitList({
+  //       firstName, email, confirmationCode: token
+  //     });
+  //     return newTokenWaitListUser.save()
+  //   }
 
-  }).then(userSaved => {
-    if (userSaved) {
-      console.log("USER SAVED: ", userSaved);
-      let newUserEmailPayload = {
-        firstName, email, token
-      }
-      emailSender(newUserEmailPayload);
-      res.status(200).json({ message: "EMAIL has been sent to user for confirmation" });
-    }
+  // }).then(userSaved => {
+  //   if (userSaved) {
+  //     console.log("USER SAVED: ", userSaved);
+  //     let newUserEmailPayload = {
+  //       firstName, email, token
+  //     }
+  //     emailSender(newUserEmailPayload);
+  //     res.status(200).json({ message: "EMAIL has been sent to user for confirmation" });
+  //   }
 
-  }).catch(err => {
-    if (err) {
-      console.log('ERROR : ', err);
-      res.status(409).json({ message: err });
-      return
-    }
-  })
+  // }).catch(err => {
+  //   if (err) {
+  //     console.log('ERROR : ', err);
+  //     res.status(409).json({ message: err });
+  //     return
+  //   }
+  // })
 
 
 });
@@ -424,15 +763,8 @@ router.get('/tokenConfirmation/:id', (req, res, next) => {
   const tokenCode = req.params.id;
   console.log('TOKEN ENTERED BY USER : ', tokenCode);
 
+  return res.status(200).json({ message: "USER HAS BEEN VERIFIED ", doc })
 
-  TokenWaitList.findOneAndUpdate({ confirmationCode: tokenCode }, { $set: { confirmed: "confirmed" } }, { new: true }, (err, doc) => {
-    if (err) {
-      console.log("Something wrong when updating data!");
-    }
-
-    console.log(doc);
-    return res.status(200).json({ message: "USER HAS BEEN VERIFIED ", doc })
-  });
 
 
 })
